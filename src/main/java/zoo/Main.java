e84a0eadc01039bb4ecf8827f5f8728916740f59
siposadam <siposadam94@gmail.com>
2021-12-27 14:32:44 +0100

    created a custom saver and loader

--- src/main/java/zoo/Main.java
+++ src/main/java/zoo/Main.java
@@ -8,40 +8,67 @@ import zoo.employee.Cleaner;
 import zoo.employee.Director;
 import zoo.employee.Employee;
 import zoo.employee.GondoZoo;
+import zoo.exception.GondozooNotAvailable;
+import zoo.exception.ZooEmployeeException;
 import zoo.task.CleanerTask;
 import zoo.task.GondoZooTask;
 import zoo.ticket.BookingThread;
 
+import java.lang.reflect.InvocationTargetException;
 import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
 		//a
 		Zoo zoo = new Zoo();
+
 		//b
 		Employee e1 = new Director("Igazgato Peter", LocalDate.of(1976, 1, 8), Sex.MALE, LocalDate.now());
 		zoo.addEmployee(e1);
+
 		//c
 		Animal a1 = new Animal(AnimalType.RHINO, "Rino", LocalDate.of(2018, 11, 22), Sex.MALE);
-		zoo.addAnimal(a1);
+		try {
+			zoo.addAnimal(a1);
+		} catch (GondozooNotAvailable e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		//d
 		Employee e2 = new GondoZoo("Gondozo Meg", LocalDate.of(1976, 1, 8), Sex.FEMALE, Arrays.asList(AnimalType.RHINO), LocalDate.of(2010, 01, 01));
 		zoo.addEmployee(e2);
+
 		//e
-		zoo.addAnimal(a1);
+		try {
+			zoo.addAnimal(a1);
+		} catch (GondozooNotAvailable e) {
+			System.out.println(e.getMessage());
+		}
+
 		//f
 		Employee e3 = new Director("Igazgato Lois", LocalDate.of(1978, 6, 8), Sex.FEMALE, LocalDate.of(2010, 01, 01));
 		zoo.addEmployee(e3);
+
 		//g
 		Employee e4 = new GondoZoo("Gondozoo Chris", LocalDate.of(1976, 1, 8), Sex.MALE, Arrays.asList(AnimalType.ELEPHANT),LocalDate.now());
 		zoo.addEmployee(e4);
+
 		//h
 		Animal a2 = new Animal(AnimalType.ELEPHANT, "Eli", LocalDate.of(2008, 3, 4), Sex.FEMALE);
-		zoo.addAnimal(a2);
+		try {
+			zoo.addAnimal(a2);
+		} catch (GondozooNotAvailable e) {
+			e.getMessage();
+		}
+
 		//i
 		zoo.showAnimals();
+
 		//j
 		zoo.sortAnimals();
 
@@ -49,12 +76,12 @@ public class Main {
 
 // 		a)
 		Employee e5 = new Cleaner("Cleaner Brian",
-					   LocalDate.of(2007, 12, 11),
+					   LocalDate.of(1978, 12, 11),
 					   Sex.MALE,
 					   Arrays.asList(CleaningArea.TERRARIUM, CleaningArea.KIFUTO),
-					   LocalDate.now());
+					   LocalDate.of(2005,11,10));
 		zoo.addEmployee(e5);
-		
+
 //		b)
 		Employee e6 = new Cleaner("Cleaner Stewie",
 					   LocalDate.of(2009, 02, 05),
@@ -62,132 +89,154 @@ public class Main {
 					   Arrays.asList(CleaningArea.KETREC, CleaningArea.MEDENCE),
 					   LocalDate.now());
 		zoo.addEmployee(e6);
+
 //		c)
 		zoo.addCompletedTask(e5, new CleanerTask(CleaningArea.KIFUTO));
 		zoo.addCompletedTask(e5, new CleanerTask(CleaningArea.TERRARIUM));
 		zoo.addCompletedTask(e2, new GondoZooTask(AnimalType.RHINO));
+
 //		d)
 		zoo.showCompletedTasks();
 
-		// PRÓBA 2/2 part
-//		DetailBox<Detail> detail2 = new DetailBox<>(
-//				new DetailAnimal("Asia", false, 17, 4, DetailAnimal.BasicAnimalClass.MAMMAL)
-//		);
+//		3.feladat 3.rész
+		DetailAnimal descriptionMammal = new DetailAnimal("Europe", false, 28, 4, DetailAnimal.BasicAnimalClass.MAMMAL);
+		DetailAnimal descriptionReptile = new DetailAnimal("Europe", true, 50, 4, DetailAnimal.BasicAnimalClass.REPTILE);
+		DetailAnimal descriptionBird = new DetailAnimal("Australia", false, 7, 2, DetailAnimal.BasicAnimalClass.BIRD);
+		DetailAnimal descriptionFish = new DetailAnimal("Africa", true, 6, 0, DetailAnimal.BasicAnimalClass.FISH);
 
-		//3.feladat 3.rész
+
 
 		VisitablePlace<DetailAnimal> visitablePlace1 = new VisitablePlace<>(
 				AnimalType.RHINO,
 				e2,
 				new Coordinate(112.242,1521.224),
-				new DetailAnimal("Europe", false, 19, 4, DetailAnimal.BasicAnimalClass.MAMMAL)
+				descriptionMammal
 		);
 		visitablePlace1.setAnimals(Arrays.asList(a1));
 
-		try {
-			zoo.addVisitablePlace(visitablePlace1);
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		zoo.addVisitablePlace(visitablePlace1);
 
 		VisitablePlace<DetailAnimal> visitablePlace2 = new VisitablePlace<>(
 				AnimalType.ELEPHANT,
-				e2,
+				e4,
 				new Coordinate(453.242,6521.224),
-				new DetailAnimal("Europe", false, 19, 4, DetailAnimal.BasicAnimalClass.MAMMAL));
+				descriptionMammal
+		);
 		visitablePlace2.setAnimals(Arrays.asList(a2));
 
-		try {
-			zoo.addVisitablePlace(visitablePlace2);
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		zoo.addVisitablePlace(visitablePlace2);
+
 
 		Employee tempEmployee = new GondoZoo(
 				"Nem itt dolgozó",
 				LocalDate.of(1993,12,07),
 				Sex.MALE,
-				Arrays.asList(AnimalType.ELEPHANT),
+				Arrays.asList(AnimalType.EAGLE),
 				LocalDate.now());
 
 		VisitablePlace<DetailAnimal> visitablePlace3 = new VisitablePlace<>(
-				AnimalType.ELEPHANT,
+				AnimalType.EAGLE,
 				tempEmployee,
 				new Coordinate(1712.242,8521.84),
-				new DetailAnimal("Africa", false, 19, 4, DetailAnimal.BasicAnimalClass.MAMMAL));
-		visitablePlace3.setAnimals(Arrays.asList(a2));
+				descriptionBird
+		);
 
+		//Error
+		//zoo.addVisitablePlace(visitablePlace3);
 
-		try {
-			zoo.addVisitablePlace(visitablePlace3);
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
 
 		VisitablePlace<DetailAnimal> visitablePlace4 = new VisitablePlace<>(
 				AnimalType.LION,
 				e2,
 				new Coordinate(6712.242,5721.84),
-				new DetailAnimal("Africa", true, 19, 4, DetailAnimal.BasicAnimalClass.MAMMAL));
+				descriptionMammal
+		);
 
-		try {
-			zoo.addVisitablePlace(visitablePlace4);
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getMessage());
-		}
+		//Error
+		//zoo.addVisitablePlace(visitablePlace4);
+
+
 
 		zoo.showSpecificAnimalsByType(AnimalType.ELEPHANT);
 
-		ZooSaver.saveZoo(zoo);
-
+		ZooSaver.saveZooPrettier(zoo);
 		//k
 		Zoo zoo2 = new Zoo();
+
 		//l
 		Zoo.showZooCount();
+
 		//m
 		Zoo.Mover mover = zoo.new Mover();
 		mover.moveZoo(zoo2);
+
 		//n
 		zoo.showAnimals();
 		zoo.showEmployees();
+
 		//o
 		zoo2.showAnimals();
 		zoo2.showEmployees();
+
 		//p
-		zoo2.releseEmployee(e1);
+		try {
+			zoo2.releaseEmployee(e1);
+		} catch (ZooEmployeeException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		//q
-		zoo2.releseEmployee(e2);
+		try {
+			zoo2.releaseEmployee(e2);
+		} catch (ZooEmployeeException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		//r
 		zoo2.sellAnimal(a1);
+
 		//s
 		zoo2.sellAnimal(a2);
+
 		//t
-		zoo2.releseEmployee(e4);
+		try {
+			zoo2.releaseEmployee(e4);
+		} catch (ZooEmployeeException e) {
+			System.out.println(e.getMessage());
+		}
+
 		//u
 		zoo2.showAnimals();
 		zoo2.showEmployees();
 
 		zoo2.addCompletedTask(e2,new GondoZooTask(AnimalType.RHINO));
-		zoo2.addCompletedTask(e2,new GondoZooTask(AnimalType.GORILLA));
 
 		zoo2.showCompletedTasks();
 
-		zoo2.reward();
+		zoo2.rewardCheck();
 
-		Zoo zoo3 = ZooSaver.loadZoo();
+		Zoo zoo3 = ZooSaver.loadZooPrettier();
 
 		zoo3.showAnimals();
 		zoo3.getEmployeeManager().showCleaners();
+//
+//		try {
+//			BookingThread.runOneThread(zoo3);
+//			BookingThread.runTwoThread(zoo3);
+//			BookingThread.runFourThread(zoo3);
+//			BookingThread.runExecutor(zoo3);
+//
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//
+////		Class<?> animalClass = Class.forName("zoo.animal.Animal");
+////		Animal animal = (Animal) animalClass.getDeclaredConstructor().newInstance();
 
-		try {
-			BookingThread.runOneThread(zoo3);
-			BookingThread.runTwoThread(zoo3);
-			BookingThread.runFourThread(zoo3);
-			BookingThread.runExecutor(zoo3);
-
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 
 	}
 }
--- src/main/java/zoo/ZooSaver.java
+++ src/main/java/zoo/ZooSaver.java
@@ -1,48 +1,217 @@
 package zoo;
 
+import org.apache.commons.lang3.StringUtils;
+import zoo.animal.Animal;
+import zoo.animal.AnimalType;
+import zoo.employee.Cleaner;
+import zoo.employee.Director;
+import zoo.employee.Employee;
+import zoo.employee.GondoZoo;
+import zoo.exception.GondozooNotAvailable;
+
 import java.io.*;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Save and load Zoo class, uses a .tmp file
  */
 public class ZooSaver implements Serializable {
 
+    /**
+     * Newest function for saving our zoo to a txt file
+     * file will be called "zoo.txt" in our root folder
+     */
+    public static void saveZooPrettier(Zoo zoo) {
+
+    StringBuilder stringBuilder = new StringBuilder("Director" + " \n");
+        if (zoo.getEmployeeManager().getDirector() != null) {
+            stringBuilder.append(employeeStringBuilder(zoo.getEmployeeManager().getDirector()));
+        } else {
+            stringBuilder.append("null");
+        }
+
+        stringBuilder.append("Employee" + "\n");
+        zoo.getEmployeeManager().getWorkers().forEach(employee -> {
+            stringBuilder.append(employeeStringBuilder(employee)).append("\n");
+        });
+
+
+        stringBuilder.append("Animals" + "\n");
+        zoo.getAnimals().forEach(
+                animal -> {
+                    stringBuilder.append(animalStringBuilder(animal));
+                });
+
+        try (BufferedWriter bufferedReader = new BufferedWriter(new FileWriter("zoo.txt"))) {
+
+           bufferedReader.write(stringBuilder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Newest function for loading our zoo from a txt file
+     * the "zoo.txt" file is in our root folder
+     */
+    public static Zoo loadZooPrettier() {
+
+        Zoo tempZoo = new Zoo();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("zoo.txt"))) {
+
+            String line = bufferedReader.readLine();
+            Employee employee;
+
+            String[] directorAttributes = bufferedReader.readLine().split(";");
+                employee = new Director(
+                        directorAttributes[0].trim(),
+                        LocalDate.parse(directorAttributes[1]),
+                        Enum.valueOf(Sex.class, directorAttributes[2]),
+                        LocalDate.parse(directorAttributes[3]));
+                tempZoo.addEmployee(employee);
+            while (!(line = bufferedReader.readLine()).equals("Animals")) {
+
+                String[] employeeAttributes = line.split(";");
+
+
+                if (employeeAttributes[0].trim().equals("G")) {
+
+                    List<AnimalType> animalTypes = Arrays.stream(employeeAttributes[4].split(",")).map(
+                            animalTypeString -> Enum.valueOf(AnimalType.class,animalTypeString)
+                    ).collect(Collectors.toList());
+
+                    employee = new GondoZoo(
+                            employeeAttributes[1],
+                            LocalDate.parse(employeeAttributes[2]),
+                            Enum.valueOf(Sex.class,employeeAttributes[3]),
+                            animalTypes,
+                            LocalDate.parse(employeeAttributes[5])
+                    );
+                } else if (employeeAttributes[0].trim().equals("C")) {
+
+                    employee = new Cleaner(
+                            employeeAttributes[1],
+                            LocalDate.parse(employeeAttributes[2]),
+                            Enum.valueOf(Sex.class,employeeAttributes[3]),
+                            Arrays.stream(employeeAttributes[4].split(",")).map(
+                                    cleaningAreaString -> Enum.valueOf(CleaningArea.class,cleaningAreaString)
+                            ).collect(Collectors.toList()),
+                            LocalDate.parse(employeeAttributes[5])
+                    );
+                }
+
+                tempZoo.addEmployee(employee);
+
+            }
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] animalAttributes = line.split(";");
+                Animal animal = new Animal(
+                        Enum.valueOf(AnimalType.class, animalAttributes[0].trim()),
+                        animalAttributes[1],
+                        LocalDate.parse(animalAttributes[2]),
+                        Enum.valueOf(Sex.class, animalAttributes[3]));
+
+                tempZoo.addAnimal(animal);
+            }
+
+
+        } catch (IOException | GondozooNotAvailable e) {
+            e.printStackTrace();
+        }
+        return tempZoo;
+    }
+
+    /**
+     * Build a readable string format for a given animal. Fields are separated by ";".
+     */
+    public static String animalStringBuilder(Animal animal) {
+        StringBuilder stringBuilder = new StringBuilder("\t");
+
+            stringBuilder
+                    .append(animal.getAnimalTypeString() + ";")
+                    .append(animal.getNickname() + ";")
+                    .append(animal.getBirthday() + ";")
+                    .append(animal.getSex() + ";")
+                    .append("\n");
+            return stringBuilder.toString();
+    }
+
+    public static String employeeStringBuilder(Employee employee) {
+        StringBuilder stringBuilder = new StringBuilder("\t");
+
+        if (employee instanceof GondoZoo) {
+            stringBuilder.append("G;");
+        } else if (employee instanceof Cleaner) {
+            stringBuilder.append("C;");
+        }
+
+        stringBuilder
+                .append(employee.getName() + ";")
+                .append(employee.getBirthDate() + ";")
+                .append(employee.getSex() + ";");
+
+        if (employee instanceof Cleaner) {
+            stringBuilder
+                    .append(
+                            StringUtils.join(((Cleaner) employee).getCleaningArea(), ",") + ";"
+                    );
+        } else if (employee instanceof GondoZoo) {
+            stringBuilder
+                    .append(
+                            StringUtils.join(((GondoZoo) employee).getCaredAnimalTypes(), ",") + ";"
+                    );
+        }
+
+        stringBuilder.append(employee.getHireDate() + ";");
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Old version for saving zoo to a .tmp file
+     */
     public static void saveZoo(Zoo zoo) {
         ObjectOutputStream oos = null;
         try {
-            oos = new ObjectOutputStream( new FileOutputStream("zoo.tmp",false) );
+            oos = new ObjectOutputStream(new FileOutputStream("zoo.tmp", false));
             oos.writeObject(zoo);
             System.out.println("Az állatkert kimentve a zoo.tmp fileba");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                oos.close();
+                if (oos != null) {
+                    oos.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     *
+     * Old version for loading zoo from a .tmp file
+     */
     public static Zoo loadZoo() {
 
         Zoo loadedZoo = new Zoo();
         ObjectInputStream ois = null;
         try {
-            ois = new ObjectInputStream( new FileInputStream("zoo.tmp") );
+            ois = new ObjectInputStream(new FileInputStream("zoo.tmp"));
             loadedZoo = (Zoo) ois.readObject();
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
             try {
-                ois.close();
+                if (ois != null) {
+                    ois.close();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
